6cb2df8170297b2521418949401ad31e1f4c6584
Ismail Hasan <ismailhasan0132@gmail.com>
2018-06-11 21:45:02 -0700

    Converted to Web-Based App
    With the decision to convert the whole program from Python to Web-based, that needed to be done. Started with just the index.php, and removed local files. All files now will be online, and data will be stored in a database.

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @Author: Ismail Hasan
+ * @Version: 0.1
+ * @Since: 6/11/2018
+ *
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>SP Logger</title>
+</head>
+
+<body>
+
+</body>
+</html>
