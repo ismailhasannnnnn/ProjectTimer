7e4370b384d26585e628be148a0198cbd3eb3edc
Ismail Hasan <ismailhasan0132@gmail.com>
2018-07-25 20:35:44 -0700

    Added the code to be able to download the timeLog for local usage.

--- mainpage.php
+++ mainpage.php
@@ -6,6 +6,7 @@
  * @Since: 6/11/2018
  *
  */
+
 include("connect.php");
 session_start();
 date_default_timezone_set("America/Los_Angeles");
@@ -14,6 +15,8 @@ if (isset($_POST['clockin'])) {
     clockIn();
 } else if (isset($_POST['clockout'])) {
     clockOut();
+}else if(isset($_POST['saveFile'])){
+    saveFile();
 }
 
 function clockIn()
@@ -40,6 +43,10 @@ function clockIn()
 
 function clockOut()
 {
+
+    /*
+     * Fills in the data that was when clocking in, which is the clockOut column. The TimeSpent column isn't filled yet.
+     */
     $name = $_SESSION['login_user'];
     $firstNameSQL = "SELECT clockIn FROM timeLogs WHERE Username = '$name' AND clockOut = '0000-00-00 00:00:00'";
     $result = mysqli_query($GLOBALS['con'], $firstNameSQL);
@@ -51,6 +58,10 @@ function clockOut()
     $timeSQL = "UPDATE timeLogs SET clockOut = '$clockOutEntry' WHERE Username = '$name' AND clockIn = '$clockInEntry'";
     mysqli_query($GLOBALS['con'], $timeSQL);
 
+    /*
+     * This is where the timeSpent column is filled in, the math is done using a SQL function.
+     */
+
     $timeSpentQuery = mysqli_query($GLOBALS['con'], "SELECT TIMESTAMPDIFF(SECOND, '$clockInEntry', '$clockOutEntry')");
     $timeSpentResult = mysqli_fetch_array($timeSpentQuery);
     $timeSpent = $timeSpentResult["TIMESTAMPDIFF(SECOND, '$clockInEntry', '$clockOutEntry')"];
@@ -59,6 +70,33 @@ function clockOut()
     $formattedTimeSpent = $formatResult["SEC_TO_TIME('$timeSpent')"];
     $timeSpentSQL = "UPDATE timeLogs SET timeSpent = '$formattedTimeSpent' WHERE Username = '$name' AND clockIn = '$clockInEntry' AND clockOut = '$clockOutEntry'";
     mysqli_query($GLOBALS['con'], $timeSpentSQL);
+
+    /*
+     * Once all the data is clocked in, and the entry is fully completed, we will export all the table data for this
+     */
+
+    $csvString = "";
+    $exportQuery = "SELECT * FROM timeLogs WHERE Username='$name'";
+    $exportResult = mysqli_query($GLOBALS['con'], $exportQuery);
+    while($row = mysqli_fetch_array($exportResult)){
+
+        $a = $row["Username"];
+        $b =  $row["clockIn"];
+        $c = $row["clockOut"];
+        $d = $row["timeSpent"];
+
+        $csvString .= "\"$a\",\"$b\",\"$c\",\"$d\"\n";
+
+    }
+
+    file_put_contents("exportedData/".$name.".csv", $csvString);
+}
+
+function saveFile(){
+    $url = "https://i.ytimg.com/vi/gvfDAcKzCco/maxresdefault.jpg";
+    $filename = basename($url);
+
+    file_put_contents($filename, file_get_contents($url));
 }
 
 ?>
@@ -85,11 +123,6 @@ function clockOut()
     </form>
 
     <form method="POST" action="mainpage.php">
-<!--        <input type='submit' value='Clock In' name='clockin'>-->
-<!--        <br>-->
-<!--        <br>-->
-<!--        <input type='submit' value='Clock Out' name='clockout'>-->
-
 
         <?php
         $name = $_SESSION['login_user'];
@@ -97,9 +130,9 @@ function clockOut()
         $resultArray = mysqli_fetch_array($queryResult);
         $clockedIn = $resultArray["ClockedIn"];
 
-        if($clockedIn == 0){
+        if ($clockedIn == 0) {
             echo "<input type='submit' value='Clock In' name='clockin'>";
-        }else if($clockedIn == 1){
+        } else if ($clockedIn == 1) {
             echo "<input type='submit' value='Clock Out' name='clockout'>";
         }
 
@@ -112,16 +145,16 @@ function clockOut()
 
 <div style="text-align: center">
     <?php
-        $name = $_SESSION['login_user'];
-        $queryResult = mysqli_query($con, "SELECT ClockedIn FROM clockLogs WHERE Username='$name'");
-        $resultArray = mysqli_fetch_array($queryResult);
-        $clockedIn = $resultArray["ClockedIn"];
+    $name = $_SESSION['login_user'];
+    $queryResult = mysqli_query($con, "SELECT ClockedIn FROM clockLogs WHERE Username='$name'");
+    $resultArray = mysqli_fetch_array($queryResult);
+    $clockedIn = $resultArray["ClockedIn"];
 
-        if($clockedIn == 0){
-            echo "Clocked out.";
-        }else if($clockedIn == 1){
-            echo "Clocked in.";
-        }
+    if ($clockedIn == 0) {
+        echo "Clocked out.";
+    } else if ($clockedIn == 1) {
+        echo "Clocked in.";
+    }
 
     ?>
 </div>
@@ -131,12 +164,24 @@ function clockOut()
     <?php
     $name = $_SESSION['login_user'];
     echo "Total time spent on project: ";
-    $timeSQL = "SELECT SEC_TO_TIME( SUM( TIME_TO_SEC( `timeSpent` ) ) ) FROM timeLogs WHERE Username='$name'";
+    $timeSQL = "SELECT SEC_TO_TIME( SUM( TIME_TO_SEC( timeSpent ) ) ) FROM timeLogs WHERE Username='$name'";
     $timeResult = mysqli_query($con, $timeSQL);
     $timeRows = mysqli_fetch_array($timeResult);
     $timeNumberRows = mysqli_num_rows($timeResult);
-    $timeTotal = $timeRows['SEC_TO_TIME( SUM( TIME_TO_SEC( `timeSpent` ) ) )'];
+    $timeTotal = $timeRows['SEC_TO_TIME( SUM( TIME_TO_SEC( timeSpent ) ) )'];
     print_r($timeTotal);
+    ?>
+</div>
+<br>
+<br>
+<div style="text-align: center;">
+    <!--    <form method="post" action="mainpage.php">-->
+    <!--        <input type="submit" value="Download" name="saveFile">-->
+    <!--    </form>-->
+    <?php
+    echo "<a href=exportedData/$name.csv download=$name.csv>
+        <button>Download</button>
+        </a>"
     ?>
 </div>
 </body>
